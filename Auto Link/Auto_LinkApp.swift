dcb9ce870a8f86e135523268ed065a5a191b1ca2
Jack <Jwkamaryt@gmail.com>
2023-07-16 13:40:28 -0500

    Initial Commit

---
+++ Auto Link/Auto_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_LinkApp.swift
+//  Auto Link
+//
+//  Created by Jack Kamaryt on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Auto_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
